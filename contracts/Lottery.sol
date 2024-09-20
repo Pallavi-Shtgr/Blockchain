c8402c723eed0e95a27950d6146087d1553d02db
Pallavi <pallavishettigar03@gmail.com>
2024-09-20 07:18:03 +0530

    Add changes

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,5 +5,16 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Lottery{
     address public mnanager;
     adress payable[] public players;
+        address payable  public winner;
+
+    constructor(){
+        mnanager=msg.sender;
+
+    }
+
+    function participate() public payable{
+        require(msg.value==1 ether,"please pay 1 ether");
+        players.push(payable(msg.sender));
+    }
 
 }
